9184468c4e42bc1c80ce0b97ec24ad896a4b2b13
Chenkail <40770208+Chenkail@users.noreply.github.com>
2020-12-23 23:52:14 -0800

    Create empty bot

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use async_std::fs;
+use async_std::fs::File;
+use async_std::path::Path;
+
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    // Set a handler for the `message` event - so that whenever a new message
+    // is received - the closure (or function) passed will be called.
+    //
+    // Event handlers are dispatched through a threadpool, and so multiple
+    // events can be dispatched simultaneously.
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            // Sending a message can fail, due to a network error, an
+            // authentication error, or lack of permissions to post in the
+            // channel, so log to stdout when some error happens, with a
+            // description of it.
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a
+    // shard is booted, and a READY payload is sent by Discord. This payload
+    // contains data like the current user's guild Ids, current user data,
+    // private channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // If token file not found, create empty file and exit
+    let token_path = Path::new("token.txt");
+    if !token_path.exists().await {
+        File::create(token_path).await.unwrap();
+        println!("Token file not found! Creating empty file and exiting");
+        return;
+    }
+
+    // Read token from file and quit if token is empty
+    let token = fs::read_to_string(token_path).await.unwrap();
+    if token.is_empty() {
+        println!("Token file is empty! Shutting down");
+        return;
+    }
+
+    // Create a new instance of the Client, logging in as a bot. This will
+    // automatically prepend your bot token with "Bot ", which is a requirement
+    // by Discord for bot users.
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
+
