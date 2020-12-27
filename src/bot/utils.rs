3c5740ed61302b7ef0721d635a49411d86050e78
Chenkail <40770208+Chenkail@users.noreply.github.com>
2020-12-27 00:13:16 -0800

    Add find_member and find_channel to utils

--- src/bot/utils.rs
+++ src/bot/utils.rs
@@ -1,85 +1,63 @@
 use log::{error, warn};
 use regex::Regex;
-use serenity::{model::prelude::ReactionType::Unicode, model::prelude::*, prelude::*, Error};
+use serenity::{Error, model::prelude::ReactionType::Unicode, model::prelude::*, prelude::*, utils::{parse_channel, parse_username}};
 use serenity::{
     framework::standard::{macros::command, Args, CommandResult},
     utils::parse_role,
 };
 
-pub(crate) async fn reply(ctx: &Context, msg: &Message, content: &String) {
-    if let Err(why) = msg.channel_id.say(&ctx.http, &content).await {
-        warn!(
-            "Failed to send message in #{} because\n{:?}",
-            msg.channel_id, why
-        );
-    }
+
+pub(crate) enum Mentionable{
+    Member,
+    Role,
+    GuildChannel,
 }
 
-pub(crate) async fn find_member(
-    ctx: &Context,
-    msg: &Message,
-    member_name: &String,
-) -> Option<Member> {
-    let member: Member;
-    if let Ok(id) = member_name.parse::<u64>() {
-        member = match msg.guild_id.unwrap().member(ctx, id).await {
-            Ok(m) => m,
-            Err(_e) => return None,
-        };
-        Some(member.to_owned())
-    } else if member_name.starts_with("<@") && member_name.ends_with(">") {
-        let re = Regex::new("[<@!>]").unwrap();
-        let member_id = re.replace_all(&member_name, "").into_owned();
+/// Searches for a member given the context, message, and user name/mention
+pub(crate) async fn find_member(ctx: &Context, msg: &Message, member_name: &String) -> Option<Member> {
+    let member_option = parse_username(&member_name);
 
-        member = match msg
-            .guild_id
-            .unwrap()
-            .member(ctx, UserId(member_id.parse::<u64>().unwrap()))
-            .await
-        {
-            Ok(m) => m,
-            Err(_e) => return None,
-        };
-
-        Some(member.to_owned())
+    let guild_members = ctx.cache.guild_field(msg.guild_id.unwrap(), |guild| guild.members.to_owned()).await.unwrap();
+    if let Some(i) = member_option {
+        // If user mention found, use it
+        let id = UserId::from(i);
+        return Some(guild_members.get(&id).unwrap().to_owned());
     } else {
-        None
+        // Otherwise try to match by role name
+        for (_, member) in guild_members {
+            if member.display_name().to_lowercase() == member_name.to_lowercase() || member.display_name().to_lowercase() == member_name.to_lowercase() {
+                return Some(member);
+            }
+        }
     }
+    
+    return None;
 }
 
-pub(crate) async fn find_channel(ctx: &Context, channel_name: String) -> Option<Channel> {
-    let channel: Channel;
-    if let Ok(id) = channel_name.parse::<u64>() {
-        let channel = match ctx.http.get_channel(id).await {
-            Ok(c) => c,
-            Err(_e) => return None,
-        };
-        Some(channel.to_owned())
-    } else if channel_name.starts_with("<#") && channel_name.ends_with(">") {
-        let re = Regex::new("[<#!>]").unwrap();
-        let channel_id = re.replace_all(&channel_name, "").into_owned();
-        channel = match ctx
-            .http
-            .get_channel(channel_id.parse::<u64>().unwrap())
-            .await
-        {
-            Ok(m) => m,
-            Err(_e) => return None,
-        };
-        Some(channel.to_owned())
+/// Searches for a channel given the context, message, and channel name/mention
+pub(crate) async fn find_channel(ctx: &Context, msg: &Message, channel_name: &String) -> Option<GuildChannel> {
+    let channel_option = parse_channel(&channel_name);
+    let channel_list = ctx.cache.guild_channels(msg.guild_id.unwrap()).await.unwrap();
+    if let Some(i) = channel_option {
+        // If role mention found, use it
+        let id = ChannelId::from(i);
+        return Some(channel_list.get(&id).unwrap().to_owned());
     } else {
-        None
+        // Otherwise try to match by role name
+        for (_, channel) in channel_list {
+            if channel.name.to_lowercase() == channel_name.to_lowercase() {
+                return Some(channel);
+            }
+        }
     }
+    
+    return None;
 }
 
-pub(crate) async fn find_role(
-    ctx: &Context,
-    msg: &Message,
-    role_name: &String,
-) -> Option<Role> {
+/// Searches for a role given the context, message, and role name/mention
+pub(crate) async fn find_role(ctx: &Context, msg: &Message, role_name: &String) -> Option<Role> {
     let role_option = parse_role(&role_name);
     let guild_roles = ctx.cache.guild_roles(msg.guild_id.unwrap()).await.unwrap();
-    let found_role: Option<Role>;
     if let Some(i) = role_option {
         // If role mention found, use it
         let id = RoleId::from(i);
