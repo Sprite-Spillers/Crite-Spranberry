cedcc9086d8de3cfdb191a31b2c2a52396fd88d8
Chenkail <40770208+Chenkail@users.noreply.github.com>
2021-02-23 15:07:34 -0800

    Fix groundhog check

--- src/sprite.rs
+++ src/sprite.rs
@@ -15,7 +15,12 @@ pub(crate) async fn octopus_check(ctx: &Context, msg: &Message) {
 
 pub(crate) async fn groundhog_check(ctx: &Context, msg: &Message) {
     // Hope you don't get stuck in a time loop
-    if msg.content.to_lowercase().contains("happy groundhog's day") {
-        let _ = msg.react(ctx, '🔄').await;
+    let r = env::var("GROUNDHOG");
+    if let Ok(id) = r {
+        if *msg.author.id.as_u64().to_string() == id
+            && msg.content.to_lowercase().contains("happy")
+            && msg.content.to_lowercase().contains("groundhog") {
+            let _ = msg.react(ctx, '🔄').await;
+        }
     }
 }
