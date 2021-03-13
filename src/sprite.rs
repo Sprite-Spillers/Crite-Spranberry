8dd644baee299725df81731b36c9efd90ef03057
Chenkail <40770208+Chenkail@users.noreply.github.com>
2021-03-13 00:34:55 -0800

    Octopus react for mentions

--- src/sprite.rs
+++ src/sprite.rs
@@ -7,7 +7,7 @@ pub(crate) async fn octopus_check(ctx: &Context, msg: &Message) {
     // Everything is octopus, Oliver
     let r = env::var("OCTOPUS");
     if let Ok(id) = r {
-        if *msg.author.id.as_u64().to_string() == id {
+        if *msg.author.id.as_u64().to_string() == id || msg.mentions.iter().any(|u| u.id.to_string() == id) {
             let _ = msg.react(ctx, '🐙').await;
         };
     }
